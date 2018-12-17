d4789328d42da2d7d752339c50086dd82a54a1c3
babariviere <babathriviere@gmail.com>
2018-12-17 14:29:16 +0100

    [graphql] do not return error if grpc server is down

--- go/src/exo_server/graphql/resolver.go
+++ go/src/exo_server/graphql/resolver.go
@@ -7,11 +7,38 @@ import (
 	"exo/user"
 	upb "exo/user/pb"
 	"io"
+	"log"
+
+	"google.golang.org/grpc"
 )
 
 type Resolver struct {
 	ProjectClient ppb.ProjectServiceClient
+	ProjectPort   string
 	UserClient    upb.UserServiceClient
+	UserPort      string
+}
+
+func (r *Resolver) Connect() error {
+	if r.UserClient == nil {
+		uconn, err := grpc.Dial(":"+r.UserPort, grpc.WithInsecure())
+		if err != nil {
+			return err
+		}
+		uclient := upb.NewUserServiceClient(uconn)
+		r.UserClient = uclient
+	}
+
+	if r.ProjectClient == nil {
+		pconn, err := grpc.Dial(":"+r.ProjectPort, grpc.WithInsecure())
+		if err != nil {
+			return err
+		}
+		pclient := ppb.NewProjectServiceClient(pconn)
+		r.ProjectClient = pclient
+	}
+
+	return nil
 }
 
 func (r *Resolver) Query() QueryResolver {
@@ -22,9 +49,17 @@ type queryResolver struct{ *Resolver }
 
 // AllProjects handle allProjects query
 func (r *queryResolver) AllProjects(ctx context.Context) ([]project.Project, error) {
+	if r.ProjectClient == nil {
+		if err := r.Connect(); err != nil {
+			log.Print(err)
+			return nil, nil // ignore error as server is down
+		}
+	}
 	stream, err := r.ProjectClient.AllProjects(ctx, &ppb.ProjectsRequest{})
 	if err != nil {
-		return nil, err
+		r.ProjectClient = nil
+		log.Print(err)
+		return nil, nil
 	}
 	projects := make([]project.Project, 0, 10)
 	for {
@@ -42,9 +77,17 @@ func (r *queryResolver) AllProjects(ctx context.Context) ([]project.Project, err
 
 // AllUsers handle allUsers query
 func (r *queryResolver) AllUsers(ctx context.Context) ([]user.User, error) {
+	if r.UserClient == nil {
+		if err := r.Connect(); err != nil {
+			log.Print(err)
+			return nil, nil // ignore error as server is down
+		}
+	}
 	stream, err := r.UserClient.AllUsers(ctx, &upb.UsersRequest{})
 	if err != nil {
-		return nil, err
+		r.UserClient = nil
+		log.Print(err)
+		return nil, nil
 	}
 	users := make([]user.User, 0, 10)
 	for {
--- go/src/exo_server/graphql/server/server.go
+++ go/src/exo_server/graphql/server/server.go
@@ -3,15 +3,12 @@
 package main
 
 import (
-	ppb "exo/project/pb"
-	upb "exo/user/pb"
 	"exo_server/graphql"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/99designs/gqlgen/handler"
-	"google.golang.org/grpc"
 )
 
 const defaultPort = "8080"
@@ -22,17 +19,11 @@ const (
 )
 
 func createResolver() (*graphql.Resolver, error) {
-	uconn, err := grpc.Dial(":"+userPort, grpc.WithInsecure())
-	if err != nil {
+	r := &graphql.Resolver{UserPort: userPort, ProjectPort: projectPort}
+	if err := r.Connect(); err != nil {
 		return nil, err
 	}
-	uclient := upb.NewUserServiceClient(uconn)
-	pconn, err := grpc.Dial(":"+projectPort, grpc.WithInsecure())
-	if err != nil {
-		return nil, err
-	}
-	pclient := ppb.NewProjectServiceClient(pconn)
-	return &graphql.Resolver{UserClient: uclient, ProjectClient: pclient}, nil
+	return r, nil
 }
 
 func main() {
